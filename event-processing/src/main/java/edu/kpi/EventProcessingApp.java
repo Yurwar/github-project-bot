7ce0b226c01f8551c0379f786e2be214ecf1c8e4
Yurii Matora <yura.matora11@gmail.com>
2020-12-18 02:59:51 +0200

    [FEAT] Add test integration to event processor
     - Configure RSocket server for event processor
     - Configure RSocket client for github integration module
     - Add sample message request-response via RSocket
     - Rename entry points for modules

---
+++ event-processing/src/main/java/edu/kpi/EventProcessingApp.java
@@ -0,0 +1,11 @@
+package edu.kpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventProcessingApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EventProcessingApp.class, args);
+    }
+}
