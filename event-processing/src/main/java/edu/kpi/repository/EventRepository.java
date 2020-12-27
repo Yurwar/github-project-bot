8064cf2eab971e0346df66e5b78c979bc4d3758b
Pavlo Pankratov <p.p.1999027@gmail.com>
2020-12-27 12:47:56 +0200

    [FEAT][WIP] updated statistic service

--- event-processing/src/main/java/edu/kpi/repository/EventRepository.java
+++ event-processing/src/main/java/edu/kpi/repository/EventRepository.java
@@ -3,7 +3,10 @@ package edu.kpi.repository;
 import edu.kpi.model.Event;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface EventRepository extends ReactiveCrudRepository<Event, Long> {
-    Flux<Event> findAll();
+    Flux<Event> findAllByActionAndRepoId(String action, Long repoId);
+
+    Mono<Event> findByActionAndIssueIdAndRepoId(String action, Long issueId, Long repoId);
 }
