9000735a45807895744683c7b2beb21132f6ab4e
Pavlo Pankratov <p.p.1999027@gmail.com>
2020-12-28 16:20:49 +0200

    [FEAT][WIP] created send statistic method

--- event-processing/src/main/java/edu/kpi/service/StatisticService.java
+++ event-processing/src/main/java/edu/kpi/service/StatisticService.java
@@ -4,8 +4,11 @@ import edu.kpi.model.data.IssueEvent;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 public interface StatisticService {
-    Mono<Long> getNumberOfIssuesByAction(String action, String repoId);
-    Mono<Long> getAverageTimeByAction(String action, String repoId);
-    Flux<IssueEvent> getUnclosedEvents();
+    Mono<Long> getNumberOfIssuesByAction(String action, String repo);
+    Mono<Long> getAverageTimeByAction(String action, String repo);
+    Mono<List<IssueEvent>> getUnclosedEvents(String repo);
+    Mono<String> getMostMentionedTopic(String repo);
 }
